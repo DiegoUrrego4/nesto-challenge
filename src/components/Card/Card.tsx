8a8fba72669467c475ab3ebac6237be11a9c1b54
Diego Urrego <diegourrg@gmail.com>
2025-10-05 19:12:23 -0500

    feat: Implement Card component and update HomePage layout

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,36 @@
+import type { FC } from 'react';
+import styles from './Card.module.scss';
+
+interface CardProps {
+  title: string;
+  type: string;
+  productName: string;
+  rate: string;
+
+  onSelectProduct?: () => void;
+}
+
+export const Card: FC<CardProps> = ({
+  title,
+  type,
+  productName,
+  rate,
+  onSelectProduct,
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.subtitle}>{type}</p>
+      </div>
+
+      <p className={styles.productName}>{productName}</p>
+
+      <p className={styles.rate}>{rate}</p>
+
+      <button className={styles.button} onClick={onSelectProduct}>
+        Select this product
+      </button>
+    </div>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
+import { Card } from '../components/Card/Card';
+import styles from './HomePage.module.scss';
+
 export const HomePage = () => {
-  return(
-      <>
-          <h1>Home Page!</h1>
-      </>
-  )
-}
+  return (
+    <div className={styles.homeContainer}>
+      <Card
+        title="Best fixed"
+        type="(type)"
+        productName="(product name)"
+        rate="1.25%"
+      />
+      <Card
+        title="Best variable"
+        type="(type)"
+        productName="(product name)"
+        rate="1.05%"
+      />
+    </div>
+  );
+};
